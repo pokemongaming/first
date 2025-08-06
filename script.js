10994931ac8a646cc2caf7fed59b4c9feaeecb35
Abhishek <abhishek260297@gmail.com>
2025-08-06 19:18:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W9B34XCCYN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-W9B34XCCYN');
+</script>
