9d5240a66b2cdc9d9d925261f71cee60142bffe9
Abhishek <abhishek260297@gmail.com>
2026-05-28 11:37:42 +0530

    jhlk

---
+++ raids/consent.js
@@ -0,0 +1,9 @@
+const s1 = document.createElement("script");
+s1.src = "https://cmp.gatekeeperconsent.com/min.js";
+s1.setAttribute("data-cfasync", "false");
+document.head.appendChild(s1);
+
+const s2 = document.createElement("script");
+s2.src = "https://the.gatekeeperconsent.com/cmp.min.js";
+s2.setAttribute("data-cfasync", "false");
+document.head.appendChild(s2);
---
+++ raids/ezoic-loader.js
@@ -0,0 +1,12 @@
+<script data-cfasync="false" src="https://cmp.gatekeeperconsent.com/min.js"></script>
+<script data-cfasync="false" src="https://the.gatekeeperconsent.com/cmp.min.js"></script>window.ezstandalone = window.ezstandalone || {};
+ezstandalone.cmd = ezstandalone.cmd || [];
+
+const s1 = document.createElement("script");
+s1.src = "//www.ezojs.com/ezoic/sa.min.js";
+s1.async = true;
+document.head.appendChild(s1);
+
+const s2 = document.createElement("script");
+s2.src = "//ezoicanalytics.com/analytics.js";
+document.head.appendChild(s2);
