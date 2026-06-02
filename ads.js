93bd1786a11e778cf1717f67f58bbd51ea879857
Abhishek <abhishek260297@gmail.com>
2026-06-02 15:55:32 +0530

    cb

--- ads.js
+++ ads.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
   if (window.adsLoaded) return;
   window.adsLoaded = true;
 
@@ -95,6 +95,67 @@
       </div>
     `;
     document.body.appendChild(ad);
+  }
+
+})();*/
+
+(function () {
+  if (window.adsLoaded) return;
+  window.adsLoaded = true;
+
+  const path = window.location.pathname;
+
+  function loadScript(src) {
+    const s = document.createElement("script");
+    s.src = src;
+    s.async = true;
+    document.body.appendChild(s);
+  }
+
+  function addBanner(key, width, height) {
+    const container = document.createElement("div");
+    container.style.textAlign = "center";
+    container.style.margin = "15px 0";
+
+    container.innerHTML = `
+      <div id="ad-${key}"></div>
+    `;
+
+    document.body.appendChild(container);
+
+    const script = document.createElement("script");
+    script.innerHTML = `
+      atOptions = {
+        'key': '${key}',
+        'format': 'iframe',
+        'height': ${height},
+        'width': ${width},
+        'params': {}
+      };
+    `;
+
+    container.appendChild(script);
+
+    loadScript(`https://www.highperformanceformat.com/${key}/invoke.js`);
+  }
+
+  // RAID PAGES
+  if (path.includes("/raids/")) {
+
+    addBanner("0876037113957a982591d9b44df13267", 300, 250);
+    addBanner("8d41df8e84cff1ed8324de1c82591191", 728, 90);
+
+    loadScript("https://pl26325102.effectivecpmnetwork.com/62/8b/c1/628bc17080aadd318aa1decc35bf6e8b.js");
+  }
+
+  // GUIDES (light ads)
+  else if (path.includes("/guides/")) {
+    addBanner("0876037113957a982591d9b44df13267", 300, 250);
+  }
+
+  // DEFAULT
+  else {
+    addBanner("0876037113957a982591d9b44df13267", 300, 250);
   }
 
 })();
